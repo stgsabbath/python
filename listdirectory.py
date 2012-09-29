0fa31a5999e7fd127c4ad87973e35b98a66040d0
root <root@sabbath-ubuntu.(none)>
2012-09-29 16:26:25 +0200

    python

---
+++ listdirectory.py
@@ -0,0 +1,4 @@
+import os
+
+lookilooki = os.listdir(r'{0}'.format(raw_input("Directory: ")))
+print "%s" % lookilooki
---
+++ test.py
@@ -0,0 +1,5 @@
+import os
+directory = raw_input("?")
+findit = os.listdir(directory)
+print "%s" % findit
+
